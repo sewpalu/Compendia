0af2c167137e6af0911aa5d00df44551ee3a8150
sewpalu <sewpalu@gmail.com>
2020-01-18 14:26:28 +0100

    Initialise Heroku server

--- server/src/source/app.d
+++ server/src/source/app.d
@@ -1,14 +1,33 @@
-import std.stdio;
+// import std.stdio;
 
-import lighttp;
+// import source.router;
 
-import source.router;
+import vibe.vibe;
 
 void main()
 {
-	auto server = new Server;
+  auto settings = new HTTPServerSettings;
+  settings.port = 8080;
+  settings.bindAddresses = ["0.0.0.0"];
+
+  readOption("port|p", &settings.port,
+      "Sets the port used for serving HTTP.");
+  readOption("bind-address|bind", &settings.bindAddresses[0],
+      "Sets the address used for serving HTTP");
+
+  auto listener = listenHTTP(settings, &parseReq);
+  runApplication();
+/*
+  auto server = new Server;
 	server.host("127.0.0.1", 8080);
 	server.router.add(new Router);
 	server.router.add(Get("hi"), new Resource("text/html", "<h1>Hello World! - 3</h1>"));
 	server.run();
+*/
 }
+
+void parseReq(HTTPServerRequest req, HTTPServerResponse res)
+{
+  res.writeBody("Hey there, I'm using Dlang!");
+}
+
