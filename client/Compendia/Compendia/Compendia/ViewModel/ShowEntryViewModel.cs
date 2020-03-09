9d235eb2b968d0663dfdbdafbb3c76161f6487e5
L301198 <lisabinkert73@gmail.com>
2020-03-09 16:24:19 +0100

    add delay

--- client/Compendia/Compendia/Compendia/ViewModel/ShowEntryViewModel.cs
+++ client/Compendia/Compendia/Compendia/ViewModel/ShowEntryViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Compendia.ViewModel
 {
@@ -23,91 +24,98 @@ namespace Compendia.ViewModel
 
         private async void LoadEntrysAsync()
         {
-            var get = await Server.GetHTTPResquestAsync();
-            // Entrys aus der Datenbank laden 
-            var templresult = await Server.GetTemplates("testuser");
-            JObject templ = JObject.Parse(templresult);
-            JArray items = (JArray)templ["result"]["templates"];
-            int length = items.Count;
-            string terminId = String.Empty;
-            string eintragId = String.Empty;
-            for (int i = 0; i < length; i++)
-            {
-                if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Termin"))
+
+                var get = await Server.GetHTTPResquestAsync();
+                // Entrys aus der Datenbank laden 
+                var templresult = await Server.GetTemplates("testuser");
+
+
+                JObject templ = JObject.Parse(templresult);
+                JArray items = (JArray)templ["result"]["templates"];
+                int length = items.Count;
+                string terminId = String.Empty;
+                string eintragId = String.Empty;
+                for (int i = 0; i < length; i++)
                 {
-                    terminId = templ["result"]["templates"][i]["templateUuid"].ToString();
-                    LoadTerminAsync(terminId);
-                    
-                }
-                else if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Eintrag"))
-                {
-                    eintragId = templ["result"]["templates"][i]["templateUuid"].ToString();
-                    LoadEintraegeAsync(eintragId);
+                    if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Termin"))
+                    {
+                        terminId = templ["result"]["templates"][i]["templateUuid"].ToString();
+                        await  Task.Delay(100);
+                        LoadTerminAsync(terminId);
+
+                    }
+                    else if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Eintrag"))
+                    {
+                        eintragId = templ["result"]["templates"][i]["templateUuid"].ToString();
+                        await Task.Delay(100);
+                        LoadEintraegeAsync(eintragId);
+
+                    }
 
                 }
-
-            }
-
+            
 
         }
         private async void  LoadTerminAsync(string terminId)
         {
-            var entries = await Server.getEntries("testuser", terminId, "2020-Mar-09 09:00:00");
-            var termine = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
+            
+                var entries = await Server.getEntries("testuser", terminId, "2020-Mar-09 09:00:00");
+                var termine = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
 
-            var items = (JArray)termine["result"]["entries"];
+                var items = (JArray)termine["result"]["entries"];
 
-            var length = items.Count;
+                var length = items.Count;
 
-            for (int i = 0; i < length; i++)
-            {
-                var termin = termine["result"]["entries"][i]["entryDefinition"];
-                termin = JObject.Parse(termin.ToString());
-                
-                
-
-                // Entrys zu Entry-Liste hinzufügen
-                Entrys.Add(new EntryModel
+                for (int i = 0; i < length; i++)
                 {
-                    Titel = termin["parameters"]["titel"].ToString(),
-                    Date = termin["parameters"]["date"].ToString(),
-                    Text = termin["parameters"]["text"].ToString()
-                }); ;
-
-            }
+                    var termin = termine["result"]["entries"][i]["entryDefinition"];
+                    termin = JObject.Parse(termin.ToString());
 
 
 
+                    // Entrys zu Entry-Liste hinzufügen
+                    Entrys.Add(new EntryModel
+                    {
+                        Titel = termin["parameters"]["titel"].ToString(),
+                        Date = termin["parameters"]["date"].ToString(),
+                        Text = termin["parameters"]["text"].ToString()
+                    }); ;
+
+                }
+
+           
+            OnPropertyChanged();
         }
 
         private async void LoadEintraegeAsync(string eintragsId)
         {
-            var entries = await Server.getEntries("testuser", eintragsId, "2020-Mar-09 09:00:00");
-            var eintaege = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
+           
 
-            var items = (JArray)eintaege["result"]["entries"];
+                var entries = await Server.getEntries("testuser", eintragsId, "2020-Mar-09 09:00:00");
+                var eintaege = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
 
-            var length = items.Count;
+                var items = (JArray)eintaege["result"]["entries"];
 
-            for (int i = 0; i < length; i++)
-            {
-                var eintrag = eintaege["result"]["entries"][i]["entryDefinition"];
-                eintrag = JObject.Parse(eintrag.ToString());
+                var length = items.Count;
 
-
-
-                // Entrys zu Entry-Liste hinzufügen
-                Entrys.Add(new EntryModel
+                for (int i = 0; i < length; i++)
                 {
-                    Titel = eintrag["parameters"]["titel"].ToString(),
-                    Date = String.Empty,
-                    Text = eintrag["parameters"]["text"].ToString()
-                }); ;
-
-            }
+                    var eintrag = eintaege["result"]["entries"][i]["entryDefinition"];
+                    eintrag = JObject.Parse(eintrag.ToString());
 
 
 
+                    // Entrys zu Entry-Liste hinzufügen
+                    Entrys.Add(new EntryModel
+                    {
+                        Titel = eintrag["parameters"]["titel"].ToString(),
+                        Date = String.Empty,
+                        Text = eintrag["parameters"]["text"].ToString()
+                    }); ;
+
+                }
+
+            OnPropertyChanged();
         }
 
 
