3c577b9a41d31060ffb4f62ea1794c8a2e26364b
L301198 <lisabinkert73@gmail.com>
2020-03-09 14:44:49 +0100

    Show Entrys

--- client/Compendia/Compendia/Compendia/ViewModel/ShowEntryViewModel.cs
+++ client/Compendia/Compendia/Compendia/ViewModel/ShowEntryViewModel.cs
@@ -36,39 +36,76 @@ namespace Compendia.ViewModel
                 if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Termin"))
                 {
                     terminId = templ["result"]["templates"][i]["templateUuid"].ToString();
+                    LoadTerminAsync(terminId);
                     
                 }
                 else if (templ["result"]["templates"][i]["templateName"].ToString().Equals("Eintrag"))
                 {
                     eintragId = templ["result"]["templates"][i]["templateUuid"].ToString();
+                    LoadEintraegeAsync(eintragId);
 
                 }
 
             }
+
+
+        }
+        private async void  LoadTerminAsync(string terminId)
+        {
             var entries = await Server.getEntries("testuser", terminId, "2020-Mar-09 09:00:00");
             var termine = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
 
-                items = (JArray)termine["result"]["entries"];
+            var items = (JArray)termine["result"]["entries"];
+
+            var length = items.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                var termin = termine["result"]["entries"][i]["entryDefinition"];
+                termin = JObject.Parse(termin.ToString());
                 
-                length = items.Count;
-               
-                for (int i = 0; i < length; i++)
-                {
-                    var termin = termine["result"]["entries"][i]["entryDefinition"].ToString();
-                    termin = termin.Remove(termin.Length-1,1);
-                   
-                    var newtermin = JToken.Parse(termin);
+                
 
                 // Entrys zu Entry-Liste hinzufügen
                 Entrys.Add(new EntryModel
-                    {
-                        Titel = newtermin["parameters"]["titel"].ToString(),
-                        Date = newtermin["parameters"]["date"].ToString(),
-                        Text = newtermin["parameters"]["text"].ToString()
-                    }); ;
+                {
+                    Titel = termin["parameters"]["titel"].ToString(),
+                    Date = termin["parameters"]["date"].ToString(),
+                    Text = termin["parameters"]["text"].ToString()
+                }); ;
 
             }
-            
+
+
+
+        }
+
+        private async void LoadEintraegeAsync(string eintragsId)
+        {
+            var entries = await Server.getEntries("testuser", eintragsId, "2020-Mar-09 09:00:00");
+            var eintaege = JObject.Parse(entries);//YYYY-MMM-DD hh:mm:ss
+
+            var items = (JArray)eintaege["result"]["entries"];
+
+            var length = items.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                var eintrag = eintaege["result"]["entries"][i]["entryDefinition"];
+                eintrag = JObject.Parse(eintrag.ToString());
+
+
+
+                // Entrys zu Entry-Liste hinzufügen
+                Entrys.Add(new EntryModel
+                {
+                    Titel = eintrag["parameters"]["titel"].ToString(),
+                    Date = String.Empty,
+                    Text = eintrag["parameters"]["text"].ToString()
+                }); ;
+
+            }
+
 
 
         }
